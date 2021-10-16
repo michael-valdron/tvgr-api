e603d8b0765422efac64c426b5186f9934ef8ec9
Michael Valdron <michael.valdron@gmail.com>
2021-10-16 00:33:46 -0400

    init.

---
+++ app/controllers/VideoGameController.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import models.VideoGameEntry
+import play.api.libs.json.Json
+import play.api.mvc._
+
+import javax.inject.{Inject, Singleton}
+import scala.collection.mutable
+
+@Singleton
+class VideoGameController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  implicit val videoGameJson = Json.format[VideoGameEntry]
+  private val videoGameList = new mutable.ListBuffer[VideoGameEntry]
+
+  def getAll: Action[AnyContent] = Action(Ok(Json.toJson(videoGameList)))
+
+  def getById(entryId: Long): Action[AnyContent] = Action {
+    val foundEntry = videoGameList.find(_.id == entryId)
+    foundEntry match {
+      case Some(entry) => Ok(Json.toJson(entry))
+      case None => NotFound
+    }
+  }
+
+  def deleteById(entryId: Long): Action[AnyContent] = Action {
+    val foundEntry = videoGameList.find(_.id == entryId)
+
+    foundEntry match {
+      case Some(entry) =>
+        videoGameList -= entry
+        Ok(Json.toJson(foundEntry))
+      case None => NotFound
+    }
+  }
+}
---
+++ app/models/VideoGameEntry.scala
@@ -0,0 +1,3 @@
+package models
+
+case class VideoGameEntry(id: Long, title: String, genre: String, description: String, releaseDate: String)

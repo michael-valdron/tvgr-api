3e1a930d0bcfb3c75c90fe122a7f720236a65908
Michael Valdron <michael.valdron@gmail.com>
2021-10-17 05:34:57 -0400

    VideoGameEntry companion object added.

--- app/models/VideoGameEntry.scala
+++ app/models/VideoGameEntry.scala
@@ -1,3 +1,9 @@
 package models
 
+import play.api.libs.json.Json
+
 case class VideoGameEntry(id: Long, title: String, genre: String, description: String, releaseDate: String)
+
+object VideoGameEntry {
+  implicit val jsonFormat = Json.format[VideoGameEntry]
+}

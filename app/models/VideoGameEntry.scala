4f6a602540906f06452fa881d56d06d4ddb3e9bf
Michael Valdron <michael.valdron@gmail.com>
2021-10-18 23:48:37 -0400

    New JSON parsing added for VideoGameEntry.

--- app/models/VideoGameEntry.scala
+++ app/models/VideoGameEntry.scala
@@ -7,8 +7,8 @@ case class VideoGameEntry(id: Long, title: String, genre: String, description: S
 object VideoGameEntry {
   implicit private val jsonFormat: OFormat[VideoGameEntry] = Json.format[VideoGameEntry]
 
-  def fromJson(json: JsValue): Option[Either[VideoGameEntry, Seq[VideoGameEntry]]] =
-    json.validate(Json.format[Either[VideoGameEntry, Seq[VideoGameEntry]]]).asOpt
+  def fromJson(json: JsValue): Option[VideoGameEntry] = json.validate.asOpt
+  def fromJsonArray(json: JsValue): Option[Seq[VideoGameEntry]] = json.validate[Seq[VideoGameEntry]].asOpt
   def toJson(entry: VideoGameEntry): JsValue = Json.toJson(entry)
   def toJson(entries: Seq[VideoGameEntry]): JsValue = Json.toJson(entries)
 }

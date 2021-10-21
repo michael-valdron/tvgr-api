90dc8ffef45a72a72aafbb201d96fbef664b640f
Michael Valdron <michael.valdron@gmail.com>
2021-10-21 02:25:13 -0400

    Typed.

--- app/models/VideoGames.scala
+++ app/models/VideoGames.scala
@@ -1,12 +1,14 @@
 package models
 
 import slick.jdbc.PostgresProfile.api._
+import slick.lifted.ProvenShape
 
 class VideoGames(tag: Tag) extends Table[VideoGameEntry](tag, "games") {
-  def id = column[Long]("id", O.PrimaryKey)
-  def title = column[String]("title")
-  def genre = column[String]("genre")
-  def description = column[String]("description")
-  def releaseDate = column[String]("release_date")
-  def * = (id, title, genre, description, releaseDate) <> ((VideoGameEntry.apply _).tupled, VideoGameEntry.unapply)
+  def id: Rep[Long] = column[Long]("id", O.PrimaryKey)
+  def title: Rep[String] = column[String]("title")
+  def genre: Rep[String] = column[String]("genre")
+  def description: Rep[String] = column[String]("description")
+  def releaseDate: Rep[String] = column[String]("release_date")
+  def * : ProvenShape[VideoGameEntry] =
+    (id, title, genre, description, releaseDate) <> ((VideoGameEntry.apply _).tupled, VideoGameEntry.unapply)
 }

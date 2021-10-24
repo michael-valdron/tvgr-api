991719d76bbd4146e1835d099c4ef098183293e8
Michael Valdron <michael.valdron@gmail.com>
2021-10-24 03:20:14 -0400

    Added starter for VideoGameEntrySpec.

---
+++ test/models/VideoGameEntrySpec.scala
@@ -0,0 +1,31 @@
+package models
+
+import org.scalatestplus.play.PlaySpec
+
+class VideoGameEntrySpec extends PlaySpec {
+
+  "VideoGameEntrySpec" should {
+    "fromJson" in {
+    }
+
+    "apply" in {
+
+    }
+
+    "toJson" in {
+
+    }
+
+    "toJson" in {
+
+    }
+
+    "unapply" in {
+
+    }
+
+    "fromJsonArray" in {
+
+    }
+  }
+}

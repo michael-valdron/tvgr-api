4371ff79640485c24bfb58af05f2a6c164c94a31
Michael Valdron <michael.valdron@gmail.com>
2021-10-23 21:21:11 -0400

    Changed test blocks to reflect changes in MyBaseFixture.

--- test/dao/VideoGameDaoTest.scala
+++ test/dao/VideoGameDaoTest.scala
@@ -11,21 +11,21 @@ class VideoGameDaoTest extends PlaySpec with ScalaFutures with MyDataFixture {
   private val dao = fetchDao[VideoGameDao](app)
 
   "testGet" should {
-    "pull a record at id = 243425" in withSetupTeardown { _ =>
+    "pull a record at id = 243425" in withSetupTeardown {
       val Some(result) = dao.get(243425).futureValue
       assert(result === testData(0))
     }
   }
 
   "testGetAll" should {
-    "pull all records" in withSetupTeardown { _ =>
+    "pull all records" in withSetupTeardown {
       val result = dao.getAll.futureValue
       assert((result diff testData) === Seq.empty)
     }
   }
 
   "testAdd" should {
-    "add new record '123456' and return it" in withSetupTeardown {_ =>
+    "add new record '123456' and return it" in withSetupTeardown {
       val entry = VideoGameEntry(123456, "A Simple Game", "Platformer",
         "A basic game for the basic gamer.", "2020-04-10")
       val Some(result) = dao.add(entry).futureValue
@@ -36,14 +36,14 @@ class VideoGameDaoTest extends PlaySpec with ScalaFutures with MyDataFixture {
   }
 
   "testDelete" should  {
-    "delete record at id = '243425' and return it" in withSetupTeardown {_ =>
+    "delete record at id = '243425' and return it" in withSetupTeardown {
       val Some(result) = dao.delete(243425).futureValue
       assert(result.id === 243425)
     }
   }
 
   "testFilterById" should {
-    "create filter by id query entity for finding record '243425' in 'games' relation" in withSetupTeardown {_ =>
+    "create filter by id query entity for finding record '243425' in 'games' relation" in withSetupTeardown {
       val db = Database.forConfig(dbPath)
       val action = dao.filterById(243425)(TableQuery[VideoGames])
       val result = db.run(action.result).futureValue

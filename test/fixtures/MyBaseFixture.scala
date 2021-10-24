c06bdfb989987888a8767f5746726d05042352f6
Michael Valdron <michael.valdron@gmail.com>
2021-10-24 02:40:41 -0400

    Removed application builder from MyBaseFixture trait.

--- test/fixtures/MyBaseFixture.scala
+++ test/fixtures/MyBaseFixture.scala
@@ -2,7 +2,6 @@ package fixtures
 
 import org.scalatest.Assertion
 import play.api.Application
-import play.api.inject.guice.GuiceApplicationBuilder
 
 import scala.reflect.ClassTag
 
@@ -14,8 +13,6 @@ trait MyBaseFixture {
     setup()
     try test finally teardown()
   }
-
-  protected def buildApp: Application = new GuiceApplicationBuilder().build()
 
   protected def fetchDao[D : ClassTag](app: Application): D = {
     val applicationToDao = Application.instanceCache[D]

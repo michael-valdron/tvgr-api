a03b82f7094586e5c5e6dfe41451cbaf86ca6644
Michael Valdron <michael.valdron@gmail.com>
2021-10-23 21:20:30 -0400

    Fixed errors and warnings found in MyBaseFixture.

--- test/fixtures/MyBaseFixture.scala
+++ test/fixtures/MyBaseFixture.scala
@@ -4,22 +4,20 @@ import org.scalatest.Assertion
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
 
+import scala.reflect.ClassTag
+
 trait MyBaseFixture {
   protected def setup(): Unit
   protected def teardown(): Unit
 
-  protected def withSetupTeardown(test: Unit => Assertion): Assertion = {
+  protected def withSetupTeardown(test: => Assertion): Assertion = {
     setup()
-    try {
-      test()
-    } finally {
-      teardown()
-    }
+    try test finally teardown()
   }
 
   protected def buildApp: Application = new GuiceApplicationBuilder().build()
 
-  protected def fetchDao[D](app: Application): D = {
+  protected def fetchDao[D : ClassTag](app: Application): D = {
     val applicationToDao = Application.instanceCache[D]
     applicationToDao(app)
   }

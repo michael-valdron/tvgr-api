ba0da398657232d752a96c4f1f50b086878751a5
Michael Valdron <michael.valdron@gmail.com>
2021-10-25 23:35:27 -0400

    SetupDb.main now uses slick.basic.DatabaseConfig.forConfig to fetch db context.

--- app/SetupDb.scala
+++ app/SetupDb.scala
@@ -1,13 +1,18 @@
 
+import com.typesafe.config.ConfigFactory
 import models.tables.{Games, Users}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import slick.basic.DatabaseConfig
+import slick.jdbc.PostgresProfile
 import slick.jdbc.PostgresProfile.api._
 
 object SetupDb {
   def main(args: Array[String]): Unit = {
-    val db = Database.forConfig(sys.env("SLICK_TEST_DBPATH"))
+    val db = DatabaseConfig.forConfig[PostgresProfile](
+      sys.env("SLICK_TEST_DBPATH"),
+      ConfigFactory.load().resolve()).db
     val users = TableQuery[Users]
     val games = TableQuery[Games]
     val setup = DBIO.seq(

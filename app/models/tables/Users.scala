8337419485eac9fb133ec9cad097ad1ccfc9f110
Michael Valdron <michael.valdron@gmail.com>
2021-10-27 01:50:40 -0400

    Companion object added with Users and PostgresProfile replaced with H2Profile.

--- app/models/tables/Users.scala
+++ app/models/tables/Users.scala
@@ -2,10 +2,12 @@ package models.tables
 
 import models.User
 import slick.lifted.{ProvenShape, Tag}
-import slick.jdbc.PostgresProfile.api._
+import slick.jdbc.H2Profile.api._
 
 class Users(tag: Tag) extends Table[User](tag, "users") {
   def username: Rep[String] = column[String]("username", O.PrimaryKey, O.Unique)
   def password: Rep[Option[String]] = column[Option[String]]("password")
   def * : ProvenShape[User] = (username, password) <> ((User.apply _).tupled, User.unapply)
 }
+
+object Users extends TableQuery[Users](new Users(_))

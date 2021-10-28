a1caae768953b4988b7fc6e751749aaaf746f9ff
Michael Valdron <michael.valdron@gmail.com>
2021-10-27 20:12:34 -0400

    Changes and added new tests to reflect changes in GameController.

--- test/controllers/GameControllerSpec.scala
+++ test/controllers/GameControllerSpec.scala
@@ -46,7 +46,7 @@ final class GameControllerSpec() extends PlaySpec with DataFixture
       val futureResult = wsCall(Call("GET", s"/v1/game/$entityId")).get()
       val status = futureResult.futureValue.status
       val result = Game.fromJson(futureResult.futureValue.json)
-      status mustEqual 200
+      assert(status == 200, futureResult.futureValue.body)
       assert(result.nonEmpty)
       result.get.id mustEqual entityId
     }
@@ -59,29 +59,62 @@ final class GameControllerSpec() extends PlaySpec with DataFixture
       val futureResult = wsCall(Call("PUT", "/v1/add")).put(newEntry)
       val status = futureResult.futureValue.status
       val result = futureResult.futureValue.json
-      status mustEqual 200
+      assert(status == 200, futureResult.futureValue.body)
       result mustEqual newEntry
+    }
+
+    "pass invalid JSON and return 400 bad request" in {
+      val newEntry = Json.parse("{\"id\":12345,\"abc\":\"123\"}")
+      val futureResult = wsCall(Call("PUT", "/v1/add")).put(newEntry)
+      val status = futureResult.futureValue.status
+      assert(status == 400, futureResult.futureValue.body)
+    }
+
+    "try to add entry with existent id and return 409 conflict" in {
+      val newEntry = Json.parse("{\"id\":243425,\"title\":\"Grim Conclusions\",\"genre\":\"Shooter\"," +
+        "\"description\":\"\",\"releaseDate\":\"2001-01-13\"}")
+      val futureResult = wsCall(Call("PUT", "/v1/add")).put(newEntry)
+      val status = futureResult.futureValue.status
+      assert(status == 409, futureResult.futureValue.body)
     }
   }
 
-  "POST '/v1/edit/243425'" must {
+  "POST '/v1/edit'" must {
     "edit entry in database and return it" in {
       val entityId = 243425
       val entityDescription = "An adventure game of the ages!"
-      val data = Json.parse("{\"title\":\"Well of Quests\",\"genre\":\"RPG\"," +
+      val data = Json.parse(s"{\"id\":$entityId,\"title\":\"Well of Quests\",\"genre\":\"RPG\"," +
         "\"description\":\"A different description.\",\"releaseDate\":\"2005-10-01\"}")
-      val futureResult = wsCall(Call("POST", s"/v1/edit/$entityId"))
-        .post(data)
+      val futureResult = wsCall(Call("POST", s"/v1/edit")).post(data)
       val status = futureResult.futureValue.status
       val result = Game.fromJson(futureResult.futureValue.json)
-      assert(status == 200, s"expected status = 200, but got status = $status")
+      assert(status == 200, futureResult.futureValue.body)
       assert(result.nonEmpty, s"result is empty")
       assert(result.get.id == entityId, s"expected id = $entityId, but got id = ${result.get.id}")
       assert(result.get.description != entityDescription, "description field was not edited")
     }
+
+    "pass invalid JSON and return 400 bad request" in {
+      val entityId = 243425
+      val entityDescription = "An adventure game of the ages!"
+      val data = Json.parse(s"{\"id\":$entityId,\"abc\":\"123\"}")
+      val futureResult = wsCall(Call("POST", s"/v1/edit")).post(data)
+      val status = futureResult.futureValue.status
+      assert(status == 400, futureResult.futureValue.body)
+    }
+
+    "try to edit entry with non-existent id and return 404 not found" in {
+      val entityId = 123456
+      val entityDescription = "An adventure game of the ages!"
+      val data = Json.parse(s"{\"id\":$entityId,\"title\":\"Well of Quests\",\"genre\":\"RPG\"," +
+        "\"description\":\"A different description.\",\"releaseDate\":\"2005-10-01\"}")
+      val futureResult = wsCall(Call("POST", s"/v1/edit")).post(data)
+      val status = futureResult.futureValue.status
+      assert(status == 404, futureResult.futureValue.body)
+    }
   }
 
-  "DELETE '/v1/remove/243425'" must {
+  "DELETE '/v1/remove/:id'" must {
     "delete entry from database and return it" in {
       val entityId = 243425
       val futureResult = wsCall(Call("DELETE", s"/v1/remove/$entityId")).delete()
@@ -89,8 +122,15 @@ final class GameControllerSpec() extends PlaySpec with DataFixture
       val expected = Json.parse(s"{\"id\":$entityId,\"title\":\"Well of Quests\",\"genre\":\"RPG\"," +
         "\"description\":\"An adventure game of the ages!\",\"releaseDate\":\"2005-10-01\"}")
       val result = futureResult.futureValue.json
-      status mustEqual 200
+      assert(status == 200, futureResult.futureValue.body)
       result mustEqual expected
+    }
+
+    "try to edit entry with non-existent id and return 404 not found" in {
+      val entityId = 123456
+      val futureResult = wsCall(Call("DELETE", s"/v1/remove/$entityId")).delete()
+      val status = futureResult.futureValue.status
+      assert(status == 404, futureResult.futureValue.body)
     }
   }
 }

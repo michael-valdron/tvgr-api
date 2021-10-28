a31604b9f579a8721eaf01787076dee95c934a38
Michael Valdron <michael.valdron@gmail.com>
2021-10-28 03:10:15 -0400

    ScalaDocs added to Table Model classes.

--- app/models/tables/Games.scala
+++ app/models/tables/Games.scala
@@ -4,6 +4,11 @@ import models.Game
 import slick.jdbc.H2Profile.api._
 import slick.lifted.ProvenShape
 
+/**
+ * Table Model of the games relation.
+ *
+ * @param tag The table tag
+ */
 class Games(tag: Tag) extends Table[Game](tag, "games") {
   def id: Rep[Long] = column[Long]("id", O.PrimaryKey)
   def title: Rep[String] = column[String]("title")
@@ -14,7 +19,19 @@ class Games(tag: Tag) extends Table[Game](tag, "games") {
     (id, title, genre, description, releaseDate) <> ((Game.apply _).tupled, Game.unapply)
 }
 
+/**
+ * Companion object `Games` for creating a `TableQuery` and
+ * creating predefined queries for querying the games relation.
+ *
+ */
 object Games extends TableQuery[Games](new Games(_)) {
+
+  /**
+   * Creates query for filtering `Game` records by their id.
+   *
+   * @param entryId The record id to find
+   * @return An instance of `Query` to perform the aforementioned query against the database
+   */
   def filterById(entryId: Long): Query[Games, Game, Seq] =
     this.filter(_.id === entryId)
 }
--- app/models/tables/Users.scala
+++ app/models/tables/Users.scala
@@ -4,13 +4,30 @@ import models.User
 import slick.lifted.{ProvenShape, Tag}
 import slick.jdbc.H2Profile.api._
 
+/**
+ * Table Model of the users relation.
+ *
+ * @param tag The table tag
+ */
 class Users(tag: Tag) extends Table[User](tag, "users") {
   def username: Rep[String] = column[String]("username", O.PrimaryKey, O.Unique)
   def password: Rep[Option[String]] = column[Option[String]]("password")
   def * : ProvenShape[User] = (username, password) <> ((User.apply _).tupled, User.unapply)
 }
 
+/**
+ * Companion object `Users` for creating a `TableQuery` and
+ * creating predefined queries for querying the users relation.
+ *
+ */
 object Users extends TableQuery[Users](new Users(_)) {
+
+  /**
+   * Creates query for filtering `User` records by their username.
+   *
+   * @param username The username of a `User` to find
+   * @return An instance of `Query` to perform the aforementioned query against the database
+   */
   def filterByUsername(username: String): Query[Users, User, Seq] =
     this.filter(_.username === username)
 }

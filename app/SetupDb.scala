284794ab42994f2320aaad5f2e734e5edbdf1b35
Michael Valdron <michael.valdron@gmail.com>
2021-10-28 03:10:30 -0400

    ScalaDocs added to SetupDb.

--- app/SetupDb.scala
+++ app/SetupDb.scala
@@ -6,7 +6,21 @@ import slick.jdbc.H2Profile.api._
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
+/**
+ * (For development mode only) Contains small script-like
+ * method using slick query entities and database entry
+ * to create relations in target database.
+ *
+ */
 object SetupDb {
+
+  /**
+   * Script-like method using slick query entities and database entry
+   * to create relations in target database. Requires 1 commandline argument
+   * for specifying path to database config under `db.conf`.
+   *
+   * @param args Commandline arguments for process (1 Required)
+   */
   def main(args: Array[String]): Unit = {
     if (args.length != 1) {
       println("usage: cmd <path.to.db.config>")
